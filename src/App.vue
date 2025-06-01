896ee153b9b455225c879cf40cd042ed8210a37c
Malik Ahsan <malikahsan13@gmail.com>
2025-06-01 23:45:30 +0500

    created Task Item compoment

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+    <h1 class="text-2xl font-bold text-gray-800 mb-4">Task Manager</h1>
+    <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+      Add Task
+    </button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
