66566b9938266b828892a7e27fd5219a4b13c892
Malik Ahsan <malikahsan13@gmail.com>
2025-06-02 17:40:46 +0500

    added Task item component

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex items-center justify-between p-4 bg-white rounded-lg shadow mb-2">
+        <input
+            type="checkbox"
+            :checked="task.completed"
+            @change="$emit('toggle-task', task.id)"
+            class="mr-2"
+        />
+        <span :class="{ 'line-through text-gray-500': TextTrackList.completed}">
+            {{ task.title }}
+        </span>
+    </div>
+    <button
+        @click="$emit('delete-task', TextTrackList.id)"
+        class="text-read-500 hover:text-red-700"
+    >Delete</button>
+</template>
+
+<script>
+export default {
+    name: 'TaskItem',
+    props: {
+        task: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
